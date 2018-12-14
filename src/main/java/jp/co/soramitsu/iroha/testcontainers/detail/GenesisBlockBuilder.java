848c6cd6f7a8cf3c5deae73ab60777802cf85fbb
Bogdan Vaneev <warchantua@gmail.com>
2018-12-14 12:38:54 +0200

    Interface fixes for rc1
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- src/main/java/jp/co/soramitsu/iroha/testcontainers/detail/GenesisBlockBuilder.java
+++ src/main/java/jp/co/soramitsu/iroha/testcontainers/detail/GenesisBlockBuilder.java
@@ -3,6 +3,7 @@ package jp.co.soramitsu.iroha.testcontainers.detail;
 import static javax.xml.bind.DatatypeConverter.parseHexBinary;
 
 import iroha.protocol.BlockOuterClass;
+import iroha.protocol.BlockOuterClass.Block;
 import iroha.protocol.Primitive.RolePermission;
 import iroha.protocol.TransactionOuterClass;
 import java.security.KeyPair;
@@ -31,8 +32,17 @@ public class GenesisBlockBuilder {
       parseHexBinary("43eeb17f0bab10dd51ab70983c25200a1742d31b3b7b54c38c34d7b827b26eed")
   );
 
-  private BlockOuterClass.Block.Payload.Builder blockPayload = BlockOuterClass.Block.Payload
+  private BlockOuterClass.Block_v1.Payload.Builder blockPayload = BlockOuterClass.Block_v1.Payload
       .newBuilder();
+
+  public GenesisBlockBuilder addDefaultPeer() {
+    return this.addTransaction(
+        Transaction.builder(null)
+            .addPeer("0.0.0.0:10001", defaultKeyPair.getPublic().getEncoded())
+            .build()
+            .build()
+    );
+  }
 
   /**
    * Add default (test) transaction to genesis block. Useful for testing.
@@ -55,14 +65,14 @@ public class GenesisBlockBuilder {
 
     if (withDefaultPeer) {
       // for multi-peer environment it is possible to disable default peer inclusion
-      builder.addPeer("0.0.0.0:10001", defaultKeyPair.getPublic().getEncoded());
+      this.addDefaultPeer();
     }
 
     builder
         // give all permissions
         .createRole(
             defaultRoleName,
-            IntStream.range(0, 43 /* check RolePermission numbers */)
+            IntStream.range(0, 45 /* check RolePermission numbers */)
                 .boxed()
                 .map(RolePermission::forNumber)
                 .collect(Collectors.toList())
@@ -107,8 +117,12 @@ public class GenesisBlockBuilder {
     blockPayload.setCreatedTime(Instant.now().toEpochMilli());
     blockPayload.setTxNumber(blockPayload.getTransactionsCount());
 
-    return BlockOuterClass.Block.newBuilder()
-        .setPayload(blockPayload.build())
+    return Block.newBuilder()
+        .setBlockV1(
+            BlockOuterClass.Block_v1.newBuilder()
+                .setPayload(blockPayload.build())
+                .build()
+        )
         .build();
   }
 }
